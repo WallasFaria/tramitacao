3e28eb4e38c5617dac8844d850e3f67e6b8e4b30
Wallas <wallas.whitemetal@gmail.com>
2011-05-18 21:16:24 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tramitacao::Application.config.secret_token = '923f71e045d8c66dc43032d416f8dee1664bd2364d24c6173b64df59cf96026bf7d983396415b9b819788ffbc23f48b77b111265b8401336b5192cba97ca2e92'
