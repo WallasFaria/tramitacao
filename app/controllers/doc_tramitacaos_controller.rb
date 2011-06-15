9d7b727b21eb1eaad4b2757d0f3b3f023d72ab3b
Wallas <wallas.whitemetal@gmail.com>
2011-06-15 18:44:31 -0300

    ajustes de links

--- app/controllers/doc_tramitacaos_controller.rb
+++ app/controllers/doc_tramitacaos_controller.rb
@@ -1,6 +1,11 @@
 class DocTramitacaosController < InheritedResources::Base
   belongs_to :documento
   before_filter :authenticate_user!
-#  load_and_authorize_resource :documento
+
   load_and_authorize_resource :doc_tramitacao, :through => :documento
+
+  def index
+    redirect_to :root
+  end
+  
 end
--- app/controllers/setors_controller.rb
+++ app/controllers/setors_controller.rb
@@ -1,4 +1,3 @@
 class SetorsController < InheritedResources::Base
   before_filter :authenticate_user!
-#  load_and_authorize_resource :setor
 end
